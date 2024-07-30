d68124385258c07b6a0b19b6ec99f9ad6c73c19c
chandra865 <shekhar777229@gmail.com>
2024-07-31 00:09:00 +0530

    setup utils files

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+const app = express()
+
+//middleware
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"})) // for json data
+app.use(express.urlencoded({extended : true, limit: "16kb"})) // url data
+app.use(express.static("public")) // image favicon pdf store for temperary
+app.use(cookieParser()) // for cookie
+
+export {app}
