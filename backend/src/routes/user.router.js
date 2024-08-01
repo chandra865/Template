252bd336bcaee72d562cdc6103064c4cd225feff
chandra865 <shekhar777229@gmail.com>
2024-08-02 00:31:51 +0530

    user register, login, logout, fuctionality implemented

--- backend/src/routes/user.router.js
+++ backend/src/routes/user.router.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser,logoutUser, refreshAccessToken} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router = Router()
 router.route("/register").post(
     upload.fields([
@@ -15,5 +17,7 @@ router.route("/register").post(
     ]),
     registerUser
 )
-
+router.route("/login").post(loginUser)
+router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 export default router
